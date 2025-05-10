f99ed09ba966d5a5f2b81aab371ca67e26068cf3
Manel Nadir <manelnad018@gmail.com>
2025-05-10 11:05:51 +0100

    Update statistiqueRoutes.js

--- routes/statistiqueRoutes.js
+++ routes/statistiqueRoutes.js
@@ -1,14 +1,22 @@
 // routes/statisticsRoutes.js
-const express = require('express');
+const express = require ('express');
 const router = express.Router();
-const statisticsController = require('../controllers/statistiqueController');
-// Potentially add authentication/authorization middleware here if needed
-// const { authenticate, authorize } = require('../middleware/authMiddleware');
+const stats= require('../controllers/statistiqueController');
+
 
 // Example: GET /api/statistics/students-per-palier
-router.get('/students-per-palier', statisticsController.getStudentsPerPalierStats);
+router.get('/students-per-palier', stats.getStudentsPerPalierStats);
 // You might want to protect this route based on user roles from your schema:
-// router.get('/students-per-palier', authenticate, authorize(['Admin', 'Chef_Département']), statisticsController.getStudentsPerPalierStats);
+ 
+
+// NEW ROUTE: For students per section
+// Example: GET /api/statistics/students-per-section 
+router.get('/students-per-section', stats.getStudentsPerSectionStats);
+
+
+//NEW ROUTE : FOR SITUATION 
+
+router.get('/handicape', stats.handleGetHandicapedCount);
 
 
 module.exports = router;
